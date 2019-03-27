5faa297d7b3c792190e841aafc8c5796c7749cb8
Steve McCoole <steve.mccoole@objectpartners.com>
2019-03-27 10:42:49 -0500

    Initial project setup.  Copy in google code styles. Set up module for code and test application for unit/android tests.

---
+++ eosiojavasoftkeysignatureprovider/src/main/java/one/block/eosiojavasoftkeysignatureprovider/SoftKeySignatureProviderPlaceholder.java
@@ -0,0 +1,16 @@
+package one.block.eosiojavasoftkeysignatureprovider;
+
+//
+// SoftKeySignatureProviderPlaceholder
+// eosiojavasoftkeysignatureprovider
+//
+// Created by mccoole on 03-27-2019
+// Copyright © 2017-2019 block.one.
+//
+
+/**
+ * This class is just here as a placeholder until coding on the project begins.
+ */
+public class SoftKeySignatureProviderPlaceholder {
+
+}
